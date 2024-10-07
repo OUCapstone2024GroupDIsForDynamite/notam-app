631331c9352875431ed1daabbe5009dbd341ad44
Jrubio121 <Jrubio062003@gmail.com>
2024-10-07 18:23:44 -0500

    NOTAM-6: Added NOTAM class, changed NotamFetcher to make a list of Notam objects, adjusted Notam card attributes to display

--- backend/utilities/NotamFetcher.py
+++ backend/utilities/NotamFetcher.py
@@ -1,19 +1,19 @@
 import requests
-
 from config import CLIENT_ID, CLIENT_SECRET, FAA_API_URL
+import json
+from objects.Notam import Notam  # Import Notam Class
 from objects.Coordinate import Coordinate
 
 class NotamFetcher:
     def __init__(self):
         self.headers = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
         }
+        self.response_format = 'geoJson'
 
-        self.response_format='geoJson'
-
-    def fetch_by_coordinate(self, coordinates, radius = 10):
-        # Returns notams based on an array of coordinates and a radius. For now, returns a string instead of a list of notams.
+    def fetch_by_coordinate(self, coordinates, radius=10):
+        # Returns notams as Notam objects based on an array of coordinates and a radius
         notams = []
 
         for coordinate in coordinates:
@@ -27,15 +27,16 @@ class NotamFetcher:
             try:
                 response = requests.get(f"{FAA_API_URL}/notams", headers=self.headers, params=params)
                 print("Status Code:", response.status_code)
-                print("Response Text:", response.text)
-
                 response.raise_for_status()
                 data = response.json()
-                current_notam_list = [item['properties']['coreNOTAMData']['notam'] for item in data['items']]
-                notams.extend(current_notam_list)
+
+                # Create Notam objects from the response data
+                for item in data['items']:
+                    notam_as_json = json.dumps(item)  # Convert the item to a JSON string
+                    notams.append(Notam(notam_as_json))  # Add Notam Object to the list
 
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while fetching NOTAMs: {e}")
                 raise e
-            
+
         return notams
--- frontend/src/components/NotamCard.js
+++ frontend/src/components/NotamCard.js
@@ -14,7 +14,7 @@ const NotamCard = () => {
 
   const handleArrivalChange = (e) => {
     setArrivalLocation(e.target.value);
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent form from submitting the default way
@@ -23,15 +23,17 @@ const NotamCard = () => {
       const response = await fetch(
         `http://localhost:5555/api/notam/${departureLocation}/${arrivalLocation}`
       );
-      console.log(response)
+      console.log(response);
       if (response.ok) {
         const data = await response.json();
-        console.log("Fetched NOTAM data:", data) // log fetched data
+        console.log("Fetched NOTAM data:", data); // log fetched data
         setNotams(data);
       } else {
         // Display error with dynamic status code and message
         const errorMessage = `Error ${response.status}: ${response.statusText}`;
-        console.log(`Error fetching NOTAM for location ${location}: Status ${response.status}`);
+        console.log(
+          `Error fetching NOTAM for location ${location}: Status ${response.status}`
+        );
         setNotams(null);
         if (response.status === 404) {
           setError("NOTAM not found for the specified location.");
@@ -55,92 +57,94 @@ const NotamCard = () => {
 
   return (
     <div className="notam-content">
-    <div>
-      <form onSubmit={handleSubmit} className="notam-form">
-        <input
-          type="text"
-          value={departureLocation}
-          onChange={handleDepartureChange}
-          placeholder="Enter Departure Airport Code"
-          className="notam-input"
-        />
-      </form>
-      <form onSubmit={handleSubmit} className="notam-form">
-        <input
-          type="text"
-          value={arrivalLocation}
-          onChange={handleArrivalChange}
-          placeholder="Enter Arrival Airport Code"
-          className="notam-input"
-        />
-      </form>
-      <form onSubmit={handleSubmit}>
-        <button type="submit" className="notam-submit-button">
-          Fetch NOTAM
-        </button>
-      </form>
-    </div>
-    {error ? (
-          <div className="notam-error">{error}</div> // Display error message inside the card
-    ) : notams && notams.length > 0 ? (
       <div>
-      <div>
-            <h1>NOTAM Data</h1>
-            <table border="1" cellPadding="10" style={{ borderCollapse: 'collapse', width: '100%' }}>
-                <thead>
-                    <tr>
-                        <th>Account ID</th>
-                        <th>Affected FIR</th>
-                        <th>Classification</th>
-                        <th>Effective Start</th>
-                        <th>Effective End</th>
-                        <th>ICAO Location</th>
-                        <th>ID</th>
-                        <th>Issued</th>
-                        <th>Last Updated</th>
-                        <th>Location</th>
-                        <th>Maximum FL</th>
-                        <th>Minimum FL</th>
-                        <th>Number</th>
-                        <th>Purpose</th>
-                        <th>Scope</th>
-                        <th>Selection Code</th>
-                        <th>Series</th>
-                        {/* <th>Text</th> */}
-                        <th>Traffic</th>
-                        <th>Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {notams.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.accountId}</td>
-                            <td>{item.affectedFIR}</td>
-                            <td>{item.classification}</td>
-                            <td>{new Date(item.effectiveStart).toLocaleDateString()}</td>
-                            <td>{new Date(item.effectiveEnd).toLocaleDateString()}</td>
-                            <td>{item.icaoLocation}</td>
-                            <td>{item.id}</td>
-                            <td>{new Date(item.issued).toLocaleDateString()}</td>
-                            <td>{new Date(item.lastUpdated).toLocaleDateString()}</td>
-                            <td>{item.location}</td>
-                            <td>{item.maximumFL}</td>
-                            <td>{item.minimumFL}</td>
-                            <td>{item.number}</td>
-                            <td>{item.purpose}</td>
-                            <td>{item.scope}</td>
-                            <td>{item.selectionCode}</td>
-                            <td>{item.series}</td>
-                            {/* <td>{item.text}</td> */}
-                            <td>{item.traffic}</td>
-                            <td>{item.type}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>      
-    </div>
-      ) :null }
+        <form onSubmit={handleSubmit} className="notam-form">
+          <input
+            type="text"
+            value={departureLocation}
+            onChange={handleDepartureChange}
+            placeholder="Enter Departure Airport Code"
+            className="notam-input"
+          />
+        </form>
+        <form onSubmit={handleSubmit} className="notam-form">
+          <input
+            type="text"
+            value={arrivalLocation}
+            onChange={handleArrivalChange}
+            placeholder="Enter Arrival Airport Code"
+            className="notam-input"
+          />
+        </form>
+        <form onSubmit={handleSubmit}>
+          <button type="submit" className="notam-submit-button">
+            Fetch NOTAM
+          </button>
+        </form>
+      </div>
+      {error ? (
+        <div className="notam-error">{error}</div> // Display error message inside the card
+      ) : notams && notams.length > 0 ? (
+        <div>
+          <h1>NOTAM Data</h1>
+          <table
+            border="1"
+            cellPadding="10"
+            style={{ borderCollapse: "collapse", width: "100%" }}
+          >
+            <thead>
+              <tr>
+                <th>Account ID</th>
+                <th>Affected FIR</th>
+                <th>Classification</th>
+                <th>Effective Start</th>
+                <th>Effective End</th>
+                <th>ICAO Location</th>
+                <th>ID</th>
+                <th>Issued</th>
+                <th>Last Updated</th>
+                <th>Location</th>
+                <th>Maximum FL</th>
+                <th>Minimum FL</th>
+                <th>Number</th>
+                <th>Purpose</th>
+                <th>Scope</th>
+                <th>Selection Code</th>
+                <th>Series</th>
+                {/* <th>Text</th> */}
+                <th>Traffic</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {notams.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.account_id}</td>
+                  <td>{item.affected_fir}</td>
+                  <td>{item.classification}</td>
+                  <td>{new Date(item.effective_start).toLocaleDateString()}</td>
+                  <td>{new Date(item.effective_end).toLocaleDateString()}</td>
+                  <td>{item.icao_location}</td>
+                  <td>{item.id}</td>
+                  <td>{new Date(item.issued).toLocaleDateString()}</td>
+                  <td>{new Date(item.last_updated).toLocaleDateString()}</td>
+                  <td>{item.location}</td>
+                  <td>{item.maximum_fl}</td>
+                  <td>{item.minimum_fl}</td>
+                  <td>{item.number}</td>
+                  <td>{item.purpose}</td>
+                  <td>{item.scope}</td>
+                  <td>{item.selection_code}</td>
+                  <td>{item.series}</td>
+                  {/* <td>{item.text}</td> */}
+                  <td>{item.traffic}</td>
+                  <td>{item.type}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </div>
   );
 };
