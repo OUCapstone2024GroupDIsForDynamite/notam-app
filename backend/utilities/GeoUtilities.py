d0e112b59109d2ae7736ae5686b10bda81e6af89
KamidzuKu <Mikhail.Kutepov-1@ou.edu>
2024-10-23 15:51:40 -0500

    [NOTAM-21] Added the build flightpath function

--- backend/utilities/GeoUtilities.py
+++ backend/utilities/GeoUtilities.py
@@ -2,6 +2,7 @@ import csv
 
 from objects.Coordinate import Coordinate
 from exceptions.AirportNotFoundError import AirportNotFoundError
+from geopy.distance import great_circle #function to calculate the distances on the earth surface between 2 coordinates
 
 class GeoUtilities():
     def __init__(self):
@@ -47,7 +48,24 @@ class GeoUtilities():
         #Returns the coordinate for the airport
         return self.icao_codes.get(airport) or self.iata_codes.get(airport) or self.faa_codes.get(airport)
         
-    def build_flight_path(self, coordinate_a, coordinate_b):
-        #Return flight path points between two coordinates
-        #Under Construction
-        return [coordinate_a, coordinate_b]
+    def build_flight_path(self, coordinate_start, coordinate_end, distance_between_points = 150):
+        """Return flight path points between two coordinates"""
+        """Under Construction"""
+        #calculate the distance between two given airports in statue miles
+        distance_between_airports = great_circle((coordinate_start.latitude, coordinate_start.longitude), (coordinate_end.latitude, coordinate_end.longitude)).miles
+
+        #calculate total number of points
+        number_of_points = int(distance_between_airports / distance_between_points)
+
+        #find the increment values for longitude and latitude
+        longitude_increment = (coordinate_end.longitude - coordinate_start.longitude) / number_of_points
+        latitude_increment = (coordinate_end.latitude - coordinate_start.latitude) / number_of_points
+
+        #calculate and add each point including the initial and final airport coordinates into a flightpath
+        flightpath_coordinates = []
+        for i in range(0, number_of_points):
+            lat = latitude_increment * i + coordinate_start.latitude
+            long = longitude_increment * i + coordinate_start.longitude
+            flightpath_coordinates.append(Coordinate(lat, long))
+        flightpath_coordinates.append(coordinate_end)
+        return flightpath_coordinates
