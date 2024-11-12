98dc6443a43e824449a0dcb45d8c0e59a93a7c38
Brian Schettler <593822+tanminivan@users.noreply.github.com>
2024-11-12 15:48:54 -0600

    [add-blank-gitignore] Adds backend/config.py, with blank values
    This file will be ignored from here on out via the root .gitignore. You should
    be good to add your API keys to backend/config.py and it shouldn't be committed
    in the future.

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+# config.py
+# You must enter a valid API ID and Secret to query the FAA's NOTAM API.
+# Failure to do so will result in a 401 Unauthorized error from the API.
+
+CLIENT_ID = ""       # Replace with your actual client ID
+CLIENT_SECRET = ""  # Replace with your actual client secret
+FAA_API_URL = "https://external-api.faa.gov/notamapi/v1"  # The base URL for the FAA NOTAM API
+
